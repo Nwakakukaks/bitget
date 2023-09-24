0a258fbeec794a4faab5ba8d440746c6e60336a4
nwakakukaks <winnernwakaku123@gmail.com>
2023-09-24 04:51:34 +0100

    icons

---
+++ frontend/src/icons/index.ts
@@ -0,0 +1,6 @@
+export * from "./mailIcon";
+export * from "./clipboardCheckIcon";
+export * from "./clipboardAddIcon";
+export * from "./externalLinkIcon";
+export * from "./googleIcon";
+export * from "./arrowRightIcon";
